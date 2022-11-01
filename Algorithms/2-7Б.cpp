f6528368373de8b5064dc83dcb8274d54485d6c7
Igor Shishigin <70200537+IgorShishi20@users.noreply.github.com>
2022-11-02 00:18:37 +0500

    Create 2-7Б.cpp

---
+++ Algorithms/2-7Б.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if (N <= 0) {
+        cout << "невозможно построить квадрат с неположительной длиной стороны" << endl;
+    }
+    else {
+        char str1[N];
+        for (int i = 0; i < N; i++) {
+            str1[i] = '*';
+        }
+        //this will be look like anything but square if the gap between strings in console is bigger than character width
+        for (int i = 0; i < N ; i++) {
+                for (int j = 0; j < N; j++) {
+                    cout << str1[j];
+                }
+                cout << endl;
+        }
+    }
+    return 0;
+}
