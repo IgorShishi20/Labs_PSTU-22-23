7f55bf0df2be80962a7f8decb5da7cb28e3f3597
Igor Shishigin <70200537+IgorShishi20@users.noreply.github.com>
2022-11-02 00:25:01 +0500

    Update 2-7Б.cpp

--- Algorithms/2-7Б.cpp
+++ Algorithms/2-7Б.cpp
@@ -8,6 +8,7 @@ int main() {
     if (N <= 0) {
         cout << "невозможно построить квадрат с неположительной длиной стороны" << endl;
     }
+    //If we have square of size 1 - we assume that it is just 1 dot 
     else {
         char str1[N];
         for (int i = 0; i < N; i++) {
