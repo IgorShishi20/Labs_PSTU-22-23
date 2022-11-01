9f7d98592b40690fef9651fd8129b9dee0d986be
Igor Shishigin <70200537+IgorShishi20@users.noreply.github.com>
2022-11-01 23:49:17 +0500

    Create 2-7.cpp

---
+++ Algorithms/2-7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if (N <= 0) {
+        cout << "невозможно построить треугольник с неположительным основанием" << endl;
+    }
+    else {
+        char str[N];
+        for (int i = 0; i < N; i++) {
+            str[i] = ' ';
+        }
+        int k = 0;
+        for (int i = 0; i < N ; i++) {
+            str[k] = '*';
+            k++;
+            for (int j = 0; j < N; j++) {
+                cout << str[j];
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
