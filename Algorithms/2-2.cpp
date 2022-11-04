47e73422005328d0fd9a417cb82d1d136220fa25
Igor Shishigin <70200537+IgorShishi20@users.noreply.github.com>
2022-11-04 14:46:42 +0500

    Update 2-2.cpp

--- Algorithms/2-2.cpp
+++ Algorithms/2-2.cpp
@@ -9,11 +9,6 @@ int main() {
         cout<<"невозможно посчитать произведение чисел при n меньше или равном 0"<<endl;
     }
     unsigned long long int result=1;
-    //if N>=21 the result will be wrong, so please - use another technique
-    if(N>=21){
-        cout<<"the result is bigger than maximum long long integer, computation will give wrong result"<<endl;
-        //actually we could use arrays and implement something like long arithmetics, but hey, stay with us till the next patch
-    }
     for(int i=1;i<N+1;i++){
         result*=i;
     }
