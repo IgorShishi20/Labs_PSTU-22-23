a69a727df4637bcff11702b195640c7b1b4faf3a
Igor Shishigin <70200537+IgorShishi20@users.noreply.github.com>
2022-11-22 22:48:05 +0500

    Add files via upload

---
+++ Algorithms/2-17.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    double x;
+    long double sum,current;
+    cout<< "Введите степень многочлена :";
+    cin >> N;
+    cout << "Введите точку, в которой хотите найти значение многочлена ";
+    cin >> x;
+    current = 1;
+    sum = 0;
+    for( int i=1;i<=N;i++){
+        sum += current;
+        current *= x/i;
+    }
+    sum += current;
+    cout << sum;
+    return 0;
+}
+// 5 +2^3/6 + 2^4/24 + 2^5/120
---
+++ Algorithms/2-21.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    float a,prev;
+    bool increasing=true;
+    cout<<"Введите последовательность "<<endl;
+    cin>>a;
+    prev = a;
+    if(a!=0){
+        do{
+            if(a<prev){
+                increasing = false;
+            }
+            cin>>a;
+        }while(a!=0);
+    }
+    if(increasing){
+        cout << "Последовательность упорядочена по возрастанию" << endl;
+    }
+    else{
+        cout<<"Последовательность не упорядочена по возрастанию"<<endl;
+    }
+    return 0;
+}
