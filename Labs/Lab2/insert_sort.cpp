504a5fd9e06154b5221d429d71a1f66556272fe1
Igor Shishigin <70200537+IgorShishi20@users.noreply.github.com>
2023-02-06 14:22:50 +0500

    Add files via upload

---
+++ Labs/Lab2/insert_sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cout<<"what size our array should have?"<<endl;
+    cin>>N;
+    int *arr;
+    arr = new int[N];
+    cout<<"Give us an array to sort"<<endl;
+    for(int i=1;i<=N;i++){
+        cin>>arr[i-1];
+    }
+    for(int i=1;i<=N;i++){
+        int tmp=arr[i];
+        for(int j=i-1;j>=1 && arr[j]>tmp;j--){
+           swap(arr[j+1],arr[j]);
+        }
+    }
+    cout<<"Sorted array is "<<endl;
+    for(int i=1;i<=N;i++){
+        cout<<arr[i-1]<<" ";
+    }
+    cout<<endl;
+    delete arr;
+    return 0;
+}
