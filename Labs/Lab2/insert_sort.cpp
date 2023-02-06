1e7490681926825d9a19ba92b656b0d13e6ab86a
Igor Shishigin <70200537+IgorShishi20@users.noreply.github.com>
2023-02-06 14:51:35 +0500

    Update insert_sort.cpp

--- Labs/Lab2/insert_sort.cpp
+++ Labs/Lab2/insert_sort.cpp
@@ -10,11 +10,14 @@ int main(){
     for(int i=1;i<=N;i++){
         cin>>arr[i-1];
     }
-    for(int i=1;i<=N;i++){
-        int tmp=arr[i];
-        for(int j=i-1;j>=1 && arr[j]>tmp;j--){
-           swap(arr[j+1],arr[j]);
+    for(int i=2;i<=N;i++){
+        int tmp = arr[i-1];
+        int j;
+        for(j=i-1;j>=1 && arr[j-1]>tmp;j--){
+            arr[j]=arr[j-1];
+            arr[j-1]=-INT32_MAX;
         }
+        arr[j]=tmp;
     }
     cout<<"Sorted array is "<<endl;
     for(int i=1;i<=N;i++){
