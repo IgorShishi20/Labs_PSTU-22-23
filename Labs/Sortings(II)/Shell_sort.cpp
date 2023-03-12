266186951cbdc93fc7a7ea56f47a728d726703be
Igor Shishigin <70200537+IgorShishi20@users.noreply.github.com>
2023-03-13 01:06:42 +0500

    Add files via upload

---
+++ Labs/Sortings(II)/Shell_sort.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+using namespace std;
+
+void ShellSort(int n, int mass[])
+{
+    int j,tmp;
+    for(int step = n/2;step>0;step=step/2) {
+        for (int i = step; i < n; i++) {
+            tmp = mass[i];
+            for (j = i; j >= step; j -= step) {
+                if (tmp < mass[j - step]) mass[j] = mass[j - step];
+                else break;
+            }
+            mass[j] = tmp;
+        }
+    }
+}
+
+int main(){
+    int N;
+    cout<<"¬ведите число элементов массива"<<endl;
+    cin>>N;
+    int* arr = new int[N];
+    cout<<"¬ведите элементы массива"<<endl;
+    for(int i=0;i<N;i++){
+        cin>>arr[i];
+    }
+    ShellSort(N,arr);
+    cout<<"–езультат работы сортировки Ўелла"<<endl;
+    for(int i=0;i<N;i++){
+        cout<<arr[i]<<' ';
+    }
+    delete[] arr;
+}
